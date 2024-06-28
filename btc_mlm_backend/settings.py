d564074c5357d4546a2b690c2172714e96e7879b
code.to.polymath <rohit.pinnacle@gmail.com>
2024-06-29 00:28:24 +0530

    203.161.57.44 allowed header

--- btc_mlm_backend/settings.py
+++ btc_mlm_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'your-secret-key-here'  # Replace with a secure secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['203.161.57.44']
 
 # Application definition
 
